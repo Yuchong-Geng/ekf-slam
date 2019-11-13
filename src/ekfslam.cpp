fd94bf4c21b296434b7e0c82455ff29c15721d63
ycgeng <ycgeng@ucdavis.edu>
2019-11-13 14:28:12 -0800

    0.0

--- src/ekfslam.cpp
+++ src/ekfslam.cpp
@@ -9,19 +9,19 @@
 // landmark_size - number of landmarks on the map
 // robot_pose_size - number of state variables to track on the robot
 // motion_noise - amount of noise to add due to motion
-EKFSLAM::EKFSLAM(unsigned int landmark_size,
-    unsigned int robot_pose_size = 3,
-    float _motion_noise = 0.1){
-
-
-}
+// EKFSLAM::EKFSLAM(unsigned int landmark_size,
+//     unsigned int robot_pose_size = 3,
+//     float _motion_noise = 0.1){
+//
+//
+// }
 
 /****** TODO *********/
 // Description: Prediction step for the EKF based off an odometry model
 // Inputs:
 // motion - struct with the control input for one time step
 EKFSLAM::Prediction(const OdoReading& motion){
-
+    
 }
 
 /****** TODO *********/
