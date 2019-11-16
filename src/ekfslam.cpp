9dee5e70c1801341f534b0db790630b3a0fc3896
ycgeng <ycgeng@ucdavis.edu>
2019-11-15 16:51:11 -0800

    11/15

--- src/ekfslam.cpp
+++ src/ekfslam.cpp
@@ -18,11 +18,11 @@ EKFSLAM::EKFSLAM(unsigned int landmark_size,
     unsigned int r_size = robot_pose_size;
     float motion_noise = _motion_noise;
     float sensor_noise = _sensor_noise;
-    mu          = VectorXd::Zero(2*l_size + r_size, 1);
-    Sigma = MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
-    robotSigma = MatrixXd::Zero(r_size, r_size);
-    mapSigma = MatrixXd::Zero(2*l_size, 2*l_size);
-    robMapSigma = MatrixXd::Zero(r_size, 2*l_size);
+    mu          = Eigen::VectorXd::Zero(2*l_size + r_size, 1);
+    Sigma       = Eigen::MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
+    robotSigma  = Eigen::MatrixXd::Zero(r_size, r_size);
+    mapSigma    = Eigen::MatrixXd::Zero(2*l_size, 2*l_size);
+    robMapSigma = Eigen::MatrixXd::Zero(r_size, 2*l_size);
 
     //write those matrices into the big matrix
     Sigma.topLeftCorner(r_size, r_size) = robotSigma;
@@ -31,15 +31,19 @@ EKFSLAM::EKFSLAM(unsigned int landmark_size,
     Sigma.bottomRightCorner(2*l_size, 2*l_size) = mapSigma;
 
     //iniilize noise matrix:
-    R = MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
+    R = Eigen::MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
     R.topLeftCorner(r_size, r_size) << motion_noise, 0, 0,
                                        0, motion_noise, 0,
                                        0, 0, motion_noise;
 
-     Q = MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
+     Q = Eigen::MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
      Q.topLeftCorner(r_size, r_size) << sensor_noise, 0, 0,
                                         0, sensor_noise, 0,
                                         0, 0, sensor_noise;
+
+    //iniilize a vector to store info about if a certain landmark has been
+    //before:
+    observedLandmarks = Eigen::VectorXd::Zero(l_size, 1);
 
 
 }
@@ -55,7 +59,7 @@ EKFSLAM::Prediction(const OdoReading& motion){
     float t  = motion.t;
     float r2 = motion.r2;
     //compute the Gtx matrix:
-    MatrixXd Gtx = MatrixXd::Zero(3, 3);
+    Eigen::MatrixXd Gtx = MatrixXd::Zero(3, 3);
     Gtx << 1, 0, -t*sin(angle + r1),
            0, 1, t*cos(angle + r1),
            0, 0, 1;
@@ -72,7 +76,7 @@ EKFSLAM::Prediction(const OdoReading& motion){
     Sigma.bottomLeftCorner(cols - 3, 3) = Sigma.topRightCorner.transpose();
     //add motion noise to the covariance matrix:
     Sigma += R;
-
+    return mu, Sigma;
 
 }
 
@@ -81,7 +85,21 @@ EKFSLAM::Prediction(const OdoReading& motion){
 // Inputs:
 // observation - vector containing all observed landmarks from a laser scanner
 EKFSLAM::Correction(const vector<LaserReading>& observation){
+    //iniilize data:
+    unsigned int id;
+    float range;
+    float angle;
+    float x;
+    float y;
+    for (const auto&one_observation: observation) {
+      id = one_observation.id;
+      range = one_observation.range;
+      angle = one_observation.bearing;
+      MatrixXd z = MatrixXd::Zero(2, 1);
+      z << range,
+           angle;
 
-    
+    }
+
 
 }
