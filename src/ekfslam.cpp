e75fd258300e5783bb50096ce140442158a2ed44
ycgeng <ycgeng@ucdavis.edu>
2019-11-16 18:39:43 -0800

    completed not testes

--- src/ekfslam.cpp
+++ src/ekfslam.cpp
@@ -36,10 +36,9 @@ EKFSLAM::EKFSLAM(unsigned int landmark_size,
                                        0, motion_noise, 0,
                                        0, 0, motion_noise;
 
-     Q = Eigen::MatrixXd::Zero(r_size+2*l_size, r_size+2*l_size);
-     Q.topLeftCorner(r_size, r_size) << sensor_noise, 0, 0,
-                                        0, sensor_noise, 0,
-                                        0, 0, sensor_noise;
+    Q = Eigen::MatrixXd::Zero(2, 2);
+    Q << 0.5, 0,
+          0,  0.5;
 
     //iniilize a vector to store info about if a certain landmark has been
     //before:
@@ -96,35 +95,68 @@ EKFSLAM::Correction(const vector<LaserReading>& observation){
     Eigen::MatrixXd H;
     int l_size;
     l_size = observedLandmarks.size();
-    H = Eigen::MatrixXd::zero(2*observation_size, 2*l_size + 3);
+    H = Eigen::MatrixXd::Zero(2, 2*l_size + 3);
+    Eigen::MatrixXd cor_Sigma, cor_mu;
+    cor_Sigma = Eigen::MatrixXd::Zero(2*l_size + 3, 2*l_size + 3);
+    cor_mu = Eigen::MatrixXd::Zero(3+2*l_size, 1);
+    cor_mu = mu;
+    cor_Sigma = Sigma;
+    //identity matrix for updating covariance matrix Sigma:
+    Eigen::MatrixXd identity;
+    identity = Eigen::MatrixXd::identity(2*l_size + 3, 2*l_size + 3);
     //iniilize z matrix
     Eigen::MatrixXd z, expectedZ;
-    z = MatrixXd::zero(2*observation_size);
-    expectedZ = MatrixXd::zero(2*observation_size);
+    z = MatrixXd::Zero(3+2*l_size, 1);
+    expectedZ = MatrixXd::Zero(3+2*l_size, 1);
     for (size_t i = 0; i < observation_size; i++) {
       auto& one_observation;
       one_observation = observation[i];
       id = one_observation.id;
       range = one_observation.range;
       angle = one_observation.bearing;
-      z(2*i) = range;
-      z(2*i + 1) = angle;
       //record landmark location if never seen before
       if (observedLandmarks(id - 1) == 0) {
         observedLandmarks(id-1) = 1;
         mu(2*id + 1) = mu(0) + range*cos(angle + mu(2)); //landmark x coordination
         mu(2*id + 2) = mu(0) + range*sin(angle + mu(2));
       }
+      //add acutal readings to z matrix:
+      z(2*i) = range;
+      z(2*i+1) = angle;
 
       Eigen::MatrixXd delta;
       delta << mu(2*id + 1) - mu(0),
               mu(2*id + 2) - mu(1);
       double q;
       q = delta.transpose()*delta; //transpose of a matrix times a matrix is a number for this case
+      //add calculated value of reading to the expectedZ matrix:
       expectedZ(2*i) = sqrt(q);
       expectedZ(2*i+1) = atan(delta(1), delta(0)) - mu(2);
+      //calculate H matrix:
+      //iniilize F matrix first:
+      Eigen::MatrixXd F;
+      F = Eigen::MatrixXd::Zero(5, 3 + 2*l_size);
+      F.block<3,3>(0,0) << 1, 0, 0,
+                           0, 1, 0,
+                           0, 0, 1;
+      F.block<2,2>(3, 1+2*id) << 1, 0,
+                                 0, 1;
+      Eigen::MatrixXd before_H;
+      before_H << -sqrt(q)*delta(0)/q, -sqrt(q)*delta(1)/q, 0, sqrt(q)*delta(0)/q, sqrt(q)*delta(1)/q,
+                  delta(1)/q,          -delta(0)/q,        -1, -delta(1)/q,        delta(0)/q;
+      H = before_H * F;
+      //kalman gain:
+      Eigen::MatrixXd K;
+      K = Eigen::MatrixXd::Zero(3+2*l_size, 2);
+      K = Sigma * H.transpose() * (H*Sigma*H.transpose() + Q).inverse();
+      cor_mu += K * (z - expectedZ);
+      cor_Sigma = (identity - K * H) * cor_Sigma;
 
     }
+
+    //update:
+    mu = cor_mu;
+    Sigma = cor_Sigma;
 
 
 }
